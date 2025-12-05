17c8f94e57d8216b653182c36b9cf266c1611354
Leonid Bobylev <l3o6@proton.me>
2025-12-05 12:17:10 +0100

    lazygit

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -9,11 +9,11 @@ return {
         },
         config = function()
             require 'git-conflict'.setup {}
-            map('n', '<leader>cn', '<Plug>(git-conflict-next-conflict)', { desc = 'Go to next conflict' })
-            map('n', '<leader>cp', '<Plug>(git-conflict-prev-conflict)', { desc = 'Go to previous conflict' })
-            map('n', '<leader>cb', '<Plug>(git-conflict-choose-both)', { desc = 'Choose both changes' })
-            map('n', '<leader>ct', '<Plug>(git-conflict-theirs)', { desc = 'Choose their changes' })
-            map('n', '<leader>co', '<Plug>(git-conflict-ours)', { desc = 'Choose our changes' })
+            map('n', '<leader>cn', ':GitConflictNextConflict<CR>', { desc = 'Go to next conflict' })
+            map('n', '<leader>cp', ':GitConflictNextConflict<CR>', { desc = 'Go to previous conflict' })
+            map('n', '<leader>cb', ':GitConflictChooseBoth<CR>', { desc = 'Choose both changes' })
+            map('n', '<leader>ct', ':GitConflictChooseTheirs<CR>', { desc = 'Choose their changes' })
+            map('n', '<leader>co', ':GitConflictChooseOurs<CR>', { desc = 'Choose our changes' })
         end,
 
     },
@@ -21,8 +21,9 @@ return {
         'lewis6991/gitsigns.nvim',
         opts = {
             on_attach = function()
-                map('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', { desc = 'Preview hunk' })
+                map('n', '<leader>gh', ':Gitsigns preview_hunk_inline<CR>', { desc = 'Preview hunk' })
                 map('n', '<leader>gb', ':Gitsigns toggle_current_line_blame<CR>', { desc = 'Toggle current line blame' })
+                map('n', '<leader>gd', ':Gitsigns diffthis<CR>', { desc = 'Diff this' })
             end
         }
     },
@@ -30,4 +31,27 @@ return {
         'linrongbin16/gitlinker.nvim',
         opts = {}
     },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        config = function()
+            vim.g.lazygit_floating_window_scaling_factor = 1
+        end,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    }
 }
